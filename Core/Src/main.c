d9bea3ec1d534841e9a14c8ca93cf778b5e53d71
Gala345 <329536003+Gala345@users.noreply.github.com>
2026-09-15 22:42:05 +0800

    还原备注

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -61,8 +61,7 @@ static void MX_TIM3_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-//bblb test
-//wbwb test
+//by gala
 void Buzzer_SetFreq(uint16_t freq , TIM_HandleTypeDef *htim) 
 {
     if (freq == 0) {
